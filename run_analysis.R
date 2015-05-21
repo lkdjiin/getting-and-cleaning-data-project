800d75eeb5eb0ca00f2acff758d84928a27d8df2
lkdjiin <xavier.nayrac@gmail.com>
2015-05-21 09:30:11 +0200

    Part 1 of assignment

---
+++ run_analysis.R
@@ -0,0 +1,64 @@
+library(plyr)
+library(dplyr)
+
+## Assignment 0.
+## Download and unzip raw data if needed.
+if(!file.exists('UCI HAR Dataset/') {
+  url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+
+  print('Downloading data.')
+  code <- download.file(url, 'dataset.zip', method='curl')
+  if(code != 0) {
+    print('Cannot download data.')
+  }
+
+  print('Unziping data')
+  unzip('dataset.zip', './')
+}
+
+## Assignment 1.
+## Merge the training and the test sets to create one data set.
+
+# Merge y_test.txt and y_train.txt together by row.
+y_test     <- read.table('UCI HAR Dataset/test/y_test.txt')
+y_train    <- read.table('UCI HAR Dataset/train/y_train.txt')
+activities <- rbind(y_test, y_train)
+rm('y_test')
+rm('y_train')
+activity_labels <- read.table('UCI HAR Dataset/activity_labels.txt')
+activities <- join(activities, activity_labels)
+activities <- tbl_df(activities)
+activities <- select(activities, activity=V2)
+rm('activity_labels')
+
+# Merge subject_test.txt and subject_train.txt together by row.
+subject_test <- read.table('UCI HAR Dataset/test/subject_test.txt')
+subject_train <- read.table('UCI HAR Dataset/train/subject_train.txt')
+subject <- rbind(subject_test, subject_train)
+rm('subject_test')
+rm('subject_train')
+subject <- tbl_df(subject)
+subject <- rename(subject, subject=V1)
+
+# Merge X_test.txt and X_train.txt together by row.
+x_test  <- read.table('UCI HAR Dataset/test/X_test.txt')
+x_train <- read.table('UCI HAR Dataset/train/X_train.txt')
+x       <- rbind(x_test, x_train)
+rm('x_test')
+rm('x_train')
+
+# Rename columns in x by features.
+features <- read.table('UCI HAR Dataset/features.txt')
+names(x) <- features$V2
+rm('features')
+
+# Merge subject, activities and x by column.
+merged_set <- cbind(subject, activities, x)
+rm('subject')
+rm('activities')
+rm('x')
+
+## Assignment 2.
+## Extracts only the measurements on the mean and standard deviation for
+## each measurement. 
+
